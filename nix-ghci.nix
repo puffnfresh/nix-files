2af1b1f26b03933d856793cf91cd8ad49c3e9f69
Brian McKenna <bmckenna@atlassian.com>
2015-12-03 10:45:00 +1100

    Simplify nix-ghci script
    Thanks copumpkin!

--- nix-ghci.nix
+++ nix-ghci.nix
@@ -2,9 +2,6 @@ with import <nixpkgs> { };
 
 writeScriptBin "nix-ghci" ''
   #!${stdenv.shell}
-  packages=""
-  for arg in $@; do
-    packages="$packages p.$arg"
-  done
-  nix-shell -p "haskellPackages.ghcWithPackages (p: [$packages ])" --command ghci
+  args="$@"
+  nix-shell -p "haskellPackages.ghcWithPackages (p: with p; [ $args ])" --command ghci
 ''
