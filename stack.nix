23727f1fbf8b25db1c717a7dccb047d8c03ca92f
Brian McKenna <brian@brianmckenna.org>
2016-07-04 13:16:52 +1000

    Add a wrapper for Stack

---
+++ stack.nix
@@ -0,0 +1,29 @@
+{ stdenv, makeWrapper, binutils, gcc, pkgconfig, haskell, stack
+, ncurses, zlib
+, extraLibs ? [ ncurses zlib ]
+}:
+
+let
+  bins = [
+    haskell.compiler.ghc7103
+    binutils
+    gcc
+    pkgconfig
+  ];
+  inherit (stdenv) lib;
+in
+stdenv.mkDerivation {
+  name = "stack-${stack.version}";
+  inherit (stack) version;
+
+  buildInputs = [ makeWrapper pkgconfig ] ++ extraLibs;
+
+  buildCommand = ''
+    makeWrapper ${stack}/bin/stack $out/bin/stack \
+      --prefix PATH : "${lib.makeBinPath bins}" \
+      --prefix PKG_CONFIG_PATH : "$PKG_CONFIG_PATH" \
+      --prefix LD_LIBRARY_PATH : "'${lib.makeLibraryPath extraLibs}'" \
+      --prefix NIX_LDFLAGS ' ' "'$NIX_LDFLAGS'" \
+      --prefix NIX_CFLAGS_COMPILE ' ' "'$NIX_CFLAGS_COMPILE'"
+  '';
+}
