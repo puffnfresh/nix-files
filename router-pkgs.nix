77fae62e80c7209024c5734de5d9d01385a72e10
Brian McKenna <bmckenna@atlassian.com>
2016-08-14 17:36:18 +1000

    Make cross-compile of rsync work again
    For some reason the pkgs attribute was removed. Sucks.

--- router-pkgs.nix
+++ router-pkgs.nix
@@ -1,11 +1,27 @@
-with (import <nixpkgs/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix> { }).armv5tel;
-
 let
+  pkgsFun = import <nixpkgs>;
+  pkgsNoParams = pkgsFun {};
+
   staticARM = drv: (drv.overrideDerivation (d: {
+    buildInputs = d.buildInputs ++ [ pkgs.libcCross.static ];
     postUnpack = ''
-        export NIX_CROSS_CFLAGS_LINK="-static $NIX_CROSS_CFLAGS_LINK"
+      export NIX_CROSS_CFLAGS_LINK="-static $NIX_CROSS_CFLAGS_LINK"
     '';
   })).crossDrv;
+
+  sheevaplugCrossSystem = {
+    crossSystem = rec {
+      config = "arm-linux-gnueabi";
+      bigEndian = false;
+      arch = "armv5te";
+      float = "soft";
+      withTLS = true;
+      libc = "glibc";
+      platform = pkgsNoParams.platforms.sheevaplug;
+      openssl.system = "linux-generic32";
+    };
+  };
+  pkgs = pkgsFun sheevaplugCrossSystem;
 in
 {
   rsync = staticARM pkgs.rsync;
