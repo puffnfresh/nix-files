b1dbec7aaf5575f8dad2c3f5a4cb450e11018c28
Brian McKenna <brian@brianmckenna.org>
2018-06-19 14:33:49 +1000

    Remove some old Nix code

--- nix-ghci.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> { };
-
-writeScriptBin "nix-ghci" ''
-  #!${stdenv.shell}
-  args="$@"
-  nix-shell -p "haskellPackages.ghcWithPackages (p: with p; [ $args ])" --command ghci
-''
