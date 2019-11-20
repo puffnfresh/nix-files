e6ba1c74f6fdfa7e6ee95e1759eb1485cb17730c
Brian McKenna <brian@brianmckenna.org>
2019-11-20 16:44:43 +0000

    Add broken openssh

--- nix-on-droid/termux-overlay.nix
+++ nix-on-droid/termux-overlay.nix
@@ -1,4 +1,12 @@
 self: super: {
+  openssh = openssh.overrideDerivation (drv: {
+    patches = [
+      (fetchpatch {
+        url = "https://raw.githubusercontent.com/termux/termux-packages/1ff10e5c361fab8faf2b107dd206f75d4be3dbbd/packages/openssh/sshd.c.patch";
+        sha256 = "12b35lid3qskyhwn7a4ls3zp06d1l8mxdhdzxpr4zbbgfi3jl7qg";
+      })
+    ];
+  });
   htop = super.htop.overrideDerivation (drv: {
     patches = [
       (self.fetchpatch {
