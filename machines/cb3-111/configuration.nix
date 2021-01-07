b863a654dd167d0bedd875f877466894ea234dc6
Brian McKenna <brian@brianmckenna.org>
2021-01-07 22:14:59 +1100

    Move CB3-111 configuration

--- machines/cb3-111/configuration.nix
+++ machines/cb3-111/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../modules/users/brian.nix
+      ../../modules/users/brian.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
