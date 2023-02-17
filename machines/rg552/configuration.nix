ff737e022ffceffccd5866cd8d374d2e7c932b97
Brian McKenna <brian@brianmckenna.org>
2023-02-17 22:06:01 +1100

    rg552: add to flake and attempt update to 6.1 kernel

--- machines/rg552/configuration.nix
+++ machines/rg552/configuration.nix
@@ -34,49 +34,121 @@
     "console=ttyS2,1500000n8"
   ];
 
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_18.override {
-    modDirVersionArg = "5.18.0-rc3";
-    argsOverride = {
-      version = "5.18.0-rc3";
-      src = pkgs.fetchFromGitHub {
-        owner = "R-ARM";
-        repo = "kernel";
-        rev = "b9fdb84537fc5d2434b79e87c81b9e73125700a1";
-        sha256 = "sha256-vsCMoD3YEUmxUv5oSGbvniEj4RRzGPsi8pkPUpsk220=";
-      };
-    };
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_1.override {
     structuredExtraConfig = {
       DRM = lib.kernel.yes;
 
       DRM_PANEL_SHARP_LS054B3SX01 = lib.kernel.yes;
 
       INPUT_JOYSTICK = lib.kernel.yes;
+      JOYSTICK_SINGLEADCJOY = lib.kernel.yes;
 
-      # not sure if these are probed
-      ROCKCHIP_MBOX = lib.kernel.yes;
-      ROCKCHIP_RGB = lib.kernel.yes;
-      ROCKCHIP_RK3066_HDMI = lib.kernel.yes;
+      # # not sure if these are probed
+      # ROCKCHIP_MBOX = lib.kernel.yes;
+      # ROCKCHIP_RGB = lib.kernel.yes;
+      # ROCKCHIP_RK3066_HDMI = lib.kernel.yes;
 
       # disables media and other things
       EMBEDDED = lib.kernel.yes;
-
-      # debug statement
-      USB_ANNOUNCE_NEW_DEVICES = lib.kernel.yes;
     };
-    kernelPatches = [
-      {
-        name = "force-host-mode";
-        patch = ./0002-rg552-force-usbdrd_dwc3_0-to-host-dr_mode.patch;
-      }
-      {
-        name = "drm-separate-panel-orientation";
-        patch = ./v3-1-3-gpu-drm-separate-panel-orientation-property-creating-and-value-setting.patch;
-      }
-      {
-        name = "rockchip-gpio-fix-dropped-interrupts";
-        patch = ./rockchip-gpio-fix-dropped-interrupts.patch;
-      }
-    ];
+    kernelPatches =
+      let
+        rev = "a95c745242893338941914421b52fecd41879af5";
+      in
+      [
+        {
+          name = "drivers-gpu-drm-panel-add-rg552-s-panel";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0000-drivers-gpu-drm-panel-add-rg552-s-panel.patch";
+            sha256 = "sha256-CsTNUuvSGMOqwY8xOv8YzFQkVc+ARB69mmWAk8d+MxI=";
+          };
+        }
+        {
+          name = "drivers-add_rg552_singleadcjoy";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0001-drivers-add_rg552_singleadcjoy.patch";
+            sha256 = "sha256-dGYRuCtTpmLjQT5JYYpzLgsf1MXqnCGBACs5quIKTjI=";
+          };
+        }
+        {
+          name = "rockchip-from-list";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0002-rockchip-from-list.patch";
+            sha256 = "sha256-UqxJpZvIHZggOrQ1vhtXklRn6GGWzkyrpoQKwaS7kyk=";
+          };
+        }
+        {
+          name = "drivers-support-chip-RTL8188FU";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0003-drivers-support-chip-RTL8188FU.patch";
+            sha256 = "sha256-mOfL7he1s6FhqipAvOy1aKMbUFmcZUouX400GVF/LaM=";
+          };
+        }
+        {
+          name = "rename_cw2015_battery";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0003-rename_cw2015_battery.patch";
+            sha256 = "sha256-NTXKAEtLnqN98sjTYtCETCVNYQ2JKVQbpiEDwYj6aes=";
+          };
+        }
+        {
+          name = "net-phy-add-support-for-motorcomm-yt8531c-phy";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0008-net-phy-add-support-for-motorcomm-yt8531c-phy.patch";
+            sha256 = "sha256-Tp4N9WJ4TxWL94s96cJ9hiNryH8AhYzmEn8bcc1b9Ag=";
+          };
+        }
+        {
+          name = "v4l2-from-list";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0011-v4l2-from-list.patch";
+            sha256 = "sha256-C0uXIpwQgH1LDBWb03PWXqFpMw0Mdk0YRKn/4mR6YcE=";
+          };
+        }
+        {
+          name = "drm-from-list";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-0020-drm-from-list.patch";
+            sha256 = "sha256-ZncPuSf8U7FUaxLlN2A1QbjferhFvu1SSr7SqFgnkws=";
+          };
+        }
+        {
+          name = "drm-rockchip";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-1000-drm-rockchip.patch";
+            sha256 = "sha256-5Zy5pnlqs5/HeRSBqHoyzbK+Wk21hVNOUN+snMi9pyo=";
+          };
+        }
+        {
+          name = "v4l2-rockchip";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-1001-v4l2-rockchip.patch";
+            sha256 = "sha256-Ux7q0hBuF+RWap2tC4m3XSQU1HjX47YHGySVpBa0928=";
+          };
+        }
+        {
+          name = "for-libreelec";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-1002-for-libreelec.patch";
+            sha256 = "sha256-uDz1Mjo+0y6PCnUX+ZPtgC28NP9TdCcUosK1e6TuCGE=";
+          };
+        }
+        # Doesn't apply:
+        # {
+        #   name = "v4l2-wip-rkvdec-hevc";
+        #   patch = pkgs.fetchpatch {
+        #     url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-2000-v4l2-wip-rkvdec-hevc.patch";
+        #     sha256 = "sha256-4Ph2VBfKE/knWIQz87Hk26esF9JFK0VcBkyFdsuK4Ow=";
+        #   };
+        # }
+        {
+          name = "v4l2-wip-iep-driver";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/batocera-linux/batocera.linux/${rev}/board/batocera/rockchip/rk3399/linux_patches/linux-2001-v4l2-wip-iep-driver.patch";
+            sha256 = "sha256-cKJ7HbAG+dg3Tkfiyb8vGhEhp6X2h6ktswXoPXPMg/E=";
+          };
+        }
+      ];
   });
 
   networking.networkmanager = {
