dea171d3ac98a1ee664a4ee53ea0a235aa5f8df6
Brian McKenna <brian@brianmckenna.org>
2023-11-07 09:30:26 +1100

    brian: change key to file instead of fetchurl

--- modules/users/brian.nix
+++ modules/users/brian.nix
@@ -4,10 +4,7 @@
     uid = 1000;
     extraGroups = [ "wheel" "dialout" ];
     openssh.authorizedKeys.keys = [
-      (builtins.readFile (builtins.fetchurl {
-        url = "https://github.com/puffnfresh.keys";
-        sha256 = "0gv8wpjxvb18fmvjvlg5ba9phqdhrmyl86qkkv8n7s7kq4dy12di";
-      }))
+      (builtins.readFile ./brian.pub)
     ];
   };
 }
