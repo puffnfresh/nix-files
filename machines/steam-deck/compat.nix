831e04af2617f40dabe5b6ee26f42e6a77587ae8
Brian McKenna <brian@brianmckenna.org>
2024-01-19 17:26:11 +1100

    teas: pass in flake-compat

--- machines/steam-deck/compat.nix
+++ machines/steam-deck/compat.nix
@@ -1,15 +1,11 @@
 { nixpkgs
 , jovian-nixos
+, flake-compat
 }:
 
 let
-  flake-compat =
-    import (fetchTarball {
-      url = "https://github.com/puffnfresh/flake-compat/archive/9de0587e74d16c450bb7fc8779b597d92ed0ef83.tar.gz";
-      sha256 = "sha256:1218802ivxyxq453rj54ln9ri6glsxnaaqbc3gg1irqp7a9p2psh";
-    });
   flake = src:
-    (flake-compat {
+    (import flake-compat {
       inherit src;
       nodeOverrides = {
         nixpkgs = flake nixpkgs;
