05789cc9850690eb29199a427b3f708ab0dfd6c7
Brian McKenna <brian@brianmckenna.org>
2024-01-19 15:39:01 +1100

    teas: use my flake-compat overrides

--- machines/steam-deck/compat.nix
+++ machines/steam-deck/compat.nix
@@ -3,15 +3,18 @@
 }:
 
 let
-  flake-compat = src:
-    (import (fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/0f9255e01c2351cc7d116c072cb317785dd33b33.tar.gz";
-    }) { inherit src; }).defaultNix;
-  flake =
-    (import ./flake.nix).outputs {
-      self = flake;
-      nixpkgs = flake-compat nixpkgs;
-      jovian-nixos = flake-compat jovian-nixos;
-    };
+  flake-compat =
+    import (fetchTarball {
+      url = "https://github.com/puffnfresh/flake-compat/archive/9de0587e74d16c450bb7fc8779b597d92ed0ef83.tar.gz";
+      sha256 = "sha256:1218802ivxyxq453rj54ln9ri6glsxnaaqbc3gg1irqp7a9p2psh";
+    });
+  flake = src:
+    (flake-compat {
+      inherit src;
+      nodeOverrides = {
+        nixpkgs = flake nixpkgs;
+        jovian-nixos = flake jovian-nixos;
+      };
+    }).defaultNix;
 in
-flake.nixosConfigurations.teas.config.system.build.toplevel
+(flake ./.).outputs.hydraJobs.steam-deck
