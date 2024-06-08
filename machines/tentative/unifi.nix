526a134608747755275e4dee1150f6626b94f53f
Brian McKenna <brian@brianmckenna.org>
2024-06-08 12:32:56 +0000

    tentative: backport MongoDB 4.4
    Unifi requires MongoDB.
    
    But the CPU of this system does not support AVX extensions, which are
    required by MongoDB 5.0
    
    We have to backport to an old version.

--- machines/tentative/unifi.nix
+++ machines/tentative/unifi.nix
@@ -5,6 +5,12 @@
 {
   services.unifi = {
     unifiPackage = pkgs.unifi;
+    mongodbPackage = pkgs.callPackage ./mongodb/4.4.nix {
+      sasl = pkgs.cyrus_sasl;
+      boost = pkgs.boost179.override { enableShared = false; };
+      inherit (pkgs.darwin) cctools;
+      inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation Security;
+    };
     openFirewall = true;
     enable = true;
   };	
