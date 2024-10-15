03155060c8f4c8585f71119e9ecb488904e52d10
Brian McKenna <brian@brianmckenna.org>
2024-10-15 10:14:32 +0000

    tentative: use MongoDB 6.0

--- machines/tentative/unifi.nix
+++ machines/tentative/unifi.nix
@@ -5,7 +5,7 @@
 {
   services.unifi = {
     unifiPackage = pkgs.unifi8;
-    mongodbPackage = pkgs.mongodb-5_0.overrideAttrs (attrs: {
+    mongodbPackage = pkgs.mongodb-6_0.overrideAttrs (attrs: {
       sconsFlags = attrs.sconsFlags ++ [ "--experimental-optimization=-sandybridge" ];
     });
     openFirewall = true;
