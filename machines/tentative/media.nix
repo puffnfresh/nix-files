dee15d32e1b757a939f203d1bff474471a504214
Brian McKenna <brian@brianmckenna.org>
2024-12-05 01:10:40 +0000

    tentative: add more insecure packages to list

--- machines/tentative/media.nix
+++ machines/tentative/media.nix
@@ -6,7 +6,10 @@
         nixpkgs.config = {
           allowUnfree = true;
           permittedInsecurePackages = [
+            "aspnetcore-runtime-6.0.36"
             "aspnetcore-runtime-wrapped-6.0.36"
+            "dotnet-sdk-6.0.428"
+            "dotnet-sdk-wrapped-6.0.428"
           ];
         };
 
