b2707022f951bb30d34d373bd72e5e6d5225e3d6
Brian McKenna <brian@brianmckenna.org>
2025-01-08 06:02:24 +0000

    tenacious: expose Hydra job in Flake

--- machines/rg552/flake.nix
+++ machines/rg552/flake.nix
@@ -3,11 +3,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
     nixosConfigurations.tenacious =
       nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [ ./configuration.nix ];
+      };
+
+    hydraJobs =
+      let
+        toplevel =
+          name: nixosConfigurations."${name}".config.system.build.toplevel;
+      in
+      {
+        tenacious = toplevel "tenacious";
       };
   };
 }
