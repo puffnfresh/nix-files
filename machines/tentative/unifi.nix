9598a1e1a92a4a4dd402fc168ae4acf59665f75d
Brian McKenna <brian@brianmckenna.org>
2025-11-01 16:22:04 +0000

    Switch to Podman for Unifi controller

--- machines/tentative/unifi.nix
+++ machines/tentative/unifi.nix
@@ -1,15 +1,33 @@
-{ pkgs
+{ config
 , ...
 }:
 
 {
-  services.unifi = {
-    unifiPackage = pkgs.unifi;
-    mongodbPackage = pkgs.mongodb-7_0.overrideAttrs (attrs: {
-      sconsFlags = attrs.sconsFlags ++ [ "--experimental-optimization=-sandybridge" ];
-    });
-    openFirewall = true;
-    enable = true;
-  };	
-  networking.firewall.allowedTCPPorts = [ 8443 ];
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      unifi-controller = {
+        image = "jacobalberty/unifi:latest";
+        autoStart = true;
+        ports = [
+          "8080:8080"
+          "8443:8443"
+          "3478:3478/udp"
+          "10001:10001/udp"
+        ];
+        volumes = ["/var/lib/unifi:/unifi"];
+      };
+    };
+  };
+
+   networking.firewall = {
+    allowedTCPPorts = [
+      8080 # Port for UAP to inform controller.
+      8843 # Port for HTTPS portal redirect, ditto.
+    ];
+    allowedUDPPorts = [
+      3478 # UDP port used for STUN.
+      10001 # UDP port used for device discovery.
+    ];
+  };
 }
