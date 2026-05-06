f2dd803ea31890ffaf8b9e565a12c56c606d9bd6
Brian McKenna <brian@brianmckenna.org>
2026-05-06 16:08:46 +1000

    kobo-clara-2e: add image

--- machines/kobo-clara-2e/flake.nix
+++ machines/kobo-clara-2e/flake.nix
@@ -30,6 +30,25 @@
       packages.x86_64-linux.uBoot =
         crossPkgs.callPackage ./pkgs/u-boot-clara2e.nix {};
 
+      packages.armv7l-linux.image =
+        let
+          targetPkgs = self.nixosConfigurations.termly.pkgs;
+          rootfs = import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
+            inherit (self.nixosConfigurations.termly) config;
+            inherit (nixpkgs) lib;
+            pkgs = targetPkgs;
+            partitionTableType = "legacy";
+            format = "raw";
+            diskSize = "auto";
+            additionalSpace = "1024M";
+          };
+        in targetPkgs.runCommand "termly.img" {} ''
+          cp ${rootfs}/nixos.img $out
+          chmod +w $out
+          dd if=${self.packages.x86_64-linux.uBoot}/u-boot.imx \
+             of=$out bs=1k seek=1 conv=notrunc
+        '';
+
       hydraJobs.kobo-clara-2e =
         self.nixosConfigurations.termly.config.system.build.toplevel;
     };
